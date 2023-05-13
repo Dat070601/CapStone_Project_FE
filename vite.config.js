b70114ef4e3141b6d2e0f86730ec1e0d80730966
unknown <datd789@gmail.com>
2023-05-13 11:35:15 +0700

    fix deploy serve

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `https://cap-stone-project-3a0putqtz-dat070601.vercel.app`,
   plugins: [react()],
   test: {
     globals: true,
